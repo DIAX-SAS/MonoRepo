e2cc1bd2fda47ff9a9eef288e14425f71905ce17
Fernando <sepulvedafernando20@gmail.com>
2025-04-09 08:39:10 -0500

    fix(diax-front): Babel only runs for test environment

--- apps/diax-front/jest.config.ts
+++ apps/diax-front/jest.config.ts
@@ -3,7 +3,8 @@ const config = {
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { configFile: require('path').resolve(__dirname, 'babel.jest.config.js') }],
+
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/diax-front',
