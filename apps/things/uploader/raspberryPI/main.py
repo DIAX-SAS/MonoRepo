ef66a92d005b02f4225f97a6fea78016f31b8a6c
Fernando <sepulvedafernando20@gmail.com>
2025-04-28 15:33:07 -0500

    refactor(raspberryPI): was changed behavior blocking in CRON.

--- apps/things/uploader/raspberryPI/main.py
+++ apps/things/uploader/raspberryPI/main.py
@@ -1,6 +1,7 @@
 """This module starts the Modbus TCP client and sends data to the IoT Core."""
 import asyncio
 import json
+import os
 from datetime import datetime, timezone
 from utils.functions import make_modbus_requests, send_multiple_to_iot_core, validate_ip
 from utils.functions import extract_epoch_day_from_epoch
@@ -15,7 +16,7 @@ async def process_single_plc(ip):
     timestamp_in_seconds = now.timestamp()
     timestamp_in_ms = int(timestamp_in_seconds * 1_000)
 
-    plc_data = await make_modbus_requests(ip)  # <--- awaiting the Modbus read
+    plc_data = await make_modbus_requests(ip)
     data_json = json.dumps({
         "epochDay": extract_epoch_day_from_epoch(timestamp_in_ms),
         "timestamp": timestamp_in_ms,
@@ -31,8 +32,15 @@ async def process_and_send_data(plc_addresses):
     tasks = []
     for _, ip in plc_addresses.items():
         tasks.append(process_single_plc(ip))
-    messages = await asyncio.gather(*tasks)  # Run all at once
+    messages = await asyncio.gather(*tasks)
     await send_multiple_to_iot_core(messages, "PIMMStateTopic")
 
+async def main_loop():
+    """Main infinite async loop."""
+    interval_seconds = int(os.environ.get("in_s", "1"))
+    while True:
+        asyncio.create_task(process_and_send_data(plc_ips))
+        await asyncio.sleep(interval_seconds)
+
 if __name__ == "__main__":
-    asyncio.run(process_and_send_data(plc_ips))
+    asyncio.run(main_loop())
