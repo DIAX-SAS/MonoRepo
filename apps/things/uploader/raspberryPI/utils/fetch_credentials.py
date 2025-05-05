d6b80e91136e9c72ff3c9c833d660911210ed3ea
Fernando <sepulvedafernando20@gmail.com>
2025-05-05 14:30:25 -0500

    fix(CD): was installed necessaries dependencies for raspberryPI and linted.

--- apps/things/uploader/raspberryPI/utils/fetch_credentials.py
+++ apps/things/uploader/raspberryPI/utils/fetch_credentials.py
@@ -1,5 +1,8 @@
-import boto3
+"""
+This script fetches the certificates from AWS Secrets Manager
+and saves them to a specified directory."""
 import os
+import boto3
 
 secret_names = {
     "AmazonRootCA1.pem": "iot/thing/raspberryPI/AmazonRootCA1.pem",
@@ -10,16 +13,16 @@ secret_names = {
 }
 
 region = os.getenv("AWS_REGION", "us-east-1")
-output_dir = "/certificates"
+OUTPUT_DIR = "/certificates"
 
-os.makedirs(output_dir, exist_ok=True)
+os.makedirs(OUTPUT_DIR, exist_ok=True)
 client = boto3.client("secretsmanager", region_name=region)
 
 for filename, secret_name in secret_names.items():
     response = client.get_secret_value(SecretId=secret_name)
     secret_string = response.get("SecretString")
     print(f"Fetched secret for {filename}")
-    with open(os.path.join(output_dir, filename), "w") as f:
+    with open(os.path.join(OUTPUT_DIR, filename), "w", encoding="utf-8") as f:
         f.write(secret_string)
 
-print(f"Certificates written to {output_dir}")
+print(f"Certificates written to {OUTPUT_DIR}")
