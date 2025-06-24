1272647996747b7086ce56f79384ff4f9b4489dd
Fernando <sepulvedafernando20@gmail.com>
2025-06-24 14:02:15 -0500

    fix(diax-canary): was changed interface of front-end

--- apps/diax-canary/src/tests/healthcheck.spec.ts
+++ apps/diax-canary/src/tests/healthcheck.spec.ts
@@ -2,16 +2,25 @@ import { test, expect } from '@playwright/test';
 import { login, logout } from '../utils/test-helper';
 
 test.describe('Authentication', () => {
-  test('User can log in with valid credentials', async ({ page }) => {
-    await login(page);
-    await expect(page.getByText('Configuración')).toBeVisible();
-    await page.getByRole('checkbox', { name: 'Live Live' }).check();
-    await expect(page.getByTestId("Disponibilidad-factor"))
-      .not.toHaveAttribute("aria-valuenow", "0", { timeout: 30000 });
-    await expect(page.getByTestId("Rendimiento-factor"))
-      .not.toHaveAttribute("aria-valuenow", "0",);
-
-    await logout(page);
+  test('Indicators are not 0s', async ({ page }) => {
+    await page.goto("/dashboard");
+    await page.waitForTimeout(10000); 
+    for (const id of [
+      'indicador-eficiencia',
+      'indicador-calidad',
+      'indicador-rendimiento',
+      'indicador-availability',
+    ]) {
+      const locator = page.getByTestId(id);
+  
+      await expect(locator).toHaveText(/^\d+(\.\d+)?$/);
+    
+      const text = await locator.textContent();
+      const value = Number(text);
+    
+      expect(value).toBeGreaterThan(0);
+    }
+    
+    
   });
-
 });
