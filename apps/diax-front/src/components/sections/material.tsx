06898159d076d61c0a4d0019835b273bb61ab827
Fernando <sepulvedafernando20@gmail.com>
2025-07-07 14:49:40 -0500

    fix(diax-front): section material was structured well

--- apps/diax-front/src/components/sections/material.tsx
+++ apps/diax-front/src/components/sections/material.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import { MultiLayerPieChart, TimeSeriesLineChart } from '../graphs/index';
 import type { GraphData } from '../../app/dashboard/dashboard.types';
-import { CollapsibleList } from "../core/CollapsibleList";
-import styles from "../../app/dashboard/styles.module.scss"
+import { CollapsibleList } from '../core/CollapsibleList';
+import styles from '../../app/dashboard/styles.module.scss';
 
 interface MaterialProps {
-  data: (GraphData['material']|undefined)[];
+  data: (GraphData['material'] | undefined)[];
 }
 
 const Material: React.FC<MaterialProps> = ({ data }) => {
@@ -13,54 +13,66 @@ const Material: React.FC<MaterialProps> = ({ data }) => {
   const selectedData = data?.[groupBy];
 
   return (
-    <div className={`${styles["cube-container"]} ${styles.Material}`}>
-      <div className={styles["title-container"]}>
+    <div className={`${styles['cube-container']} ${styles.Material}`}>
+      <div className={styles['title-container']}>
         <h2 className={`${styles.h2}`}>Material</h2>
         <div className={styles.columns}>
-          <div className={`${styles["button-minimize-down"]} ${styles["button-main"]} ${styles.center}`}>
+          <div
+            className={`${styles['button-minimize-down']} ${styles['button-main']} ${styles.center}`}
+          >
             <div />
           </div>
         </div>
       </div>
-
-      <div className={`${styles.center} ${styles["full-width"]} ${styles.rows}`}>
+      <div
+        className={`${styles.rows} ${styles.center} ${styles['material-plc-cont']}`}
+      >
+        <h2 className={`${styles.h2}`}>Agrupación</h2>
+        <select
+          className={`${styles['material-plc']} ${styles.select}`}
+          value={groupBy}
+          onChange={(e) => setGroupBy(Number(e.target.value))}
+        >
+          <option className={`${styles.option}`} value={0}>
+            PIMM
+          </option>
+          <option className={`${styles.option}`} value={1}>
+            Molde
+          </option>
+        </select>
+      </div>
+      <div
+        className={`${styles.rows} ${styles.center} ${styles['full-width']}`}
+      >
+        <div className={`${styles['pie-container']} ${styles['margin-gd']}`}>
+          <div className={styles['pie-material']}>
+            {selectedData?.MultiPie ? (
+              <MultiLayerPieChart data={selectedData.MultiPie} unit="gramos" />
+            ) : (
+              <p>No hay datos de pastel disponibles</p>
+            )}
+          </div>
+        </div>
+      </div>
+      <div
+        className={`${styles.center} ${styles['full-width']} ${styles.rows}`}
+      >
         <ul className={`${styles.legend} ${styles.ul}`}></ul>
 
-        <div className={styles["line-container"]}>
-          <div className={styles["line-material"]}>
+        <div className={styles['line-container']}>
+          <div className={styles['line-material']}>
             {selectedData?.MultiLine ? (
               <>
-                <TimeSeriesLineChart series={selectedData.MultiLine} labelY="gramos" />
+                <TimeSeriesLineChart
+                  series={selectedData.MultiLine}
+                  labelY="gramos"
+                />
                 <CollapsibleList data={selectedData?.MultiPie} unit="gramos" />
               </>
             ) : (
               <p>No hay datos de línea disponibles</p>
             )}
           </div>
-        </div>
-
-        <div className={`${styles.rows} ${styles.center} ${styles["full-width"]}`}>
-          <div className={`${styles["pie-container"]} ${styles["margin-gd"]}`}>
-            <div className={styles["pie-material"]}>
-              {selectedData?.MultiPie ? (
-                <MultiLayerPieChart data={selectedData.MultiPie} unit="gramos" />
-              ) : (
-                <p>No hay datos de pastel disponibles</p>
-              )}
-            </div>
-          </div>
-        </div>
-
-        <div className={`${styles.rows} ${styles.center} ${styles["material-plc-cont"]}`}>
-          <h2 className={`${styles.h2}`}>Agrupación</h2>
-          <select
-            className={`${styles["material-plc"]} ${styles.select}`}
-            value={groupBy}
-            onChange={(e) => setGroupBy(Number(e.target.value))}
-          >
-            <option className={`${styles.option}`} value={0}>PIMM</option>
-            <option className={`${styles.option}`} value={1}>Molde</option>
-          </select>
         </div>
       </div>
     </div>
