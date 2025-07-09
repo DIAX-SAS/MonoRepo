5182b19c14a729e1c2eb76dbc2625838f057e569
Fernando <sepulvedafernando20@gmail.com>
2025-07-09 07:42:07 -0500

    test(diax-front-e2e): was added test for "live"

--- apps/diax-front-e2e/src/tests/navigation.spec.ts
+++ apps/diax-front-e2e/src/tests/navigation.spec.ts
@@ -33,4 +33,25 @@ test.describe('Navigation', () => {
     await page.goto('/redirect');
     await expect(page).toHaveURL('/dashboard');
   });
+
+  test('Indicators are not 0s', async ({ page }) => {
+    await page.goto("/dashboard");
+    await page.waitForTimeout(10000); 
+    for (const id of [
+      'indicador-eficiencia',
+      'indicador-calidad',
+      'indicador-rendimiento',
+      'indicador-availability',
+    ]) {
+      const locator = page.getByTestId(id);
+  
+      await expect(locator).toHaveText(/^\d+(\.\d+)?$/);
+    
+      const text = await locator.textContent();
+      const value = Number(text);
+    
+      expect(value).toBeGreaterThan(0);
+    }    
+    
+  });
 });
